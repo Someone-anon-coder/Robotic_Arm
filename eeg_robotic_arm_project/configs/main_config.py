6519d930f5403366abfb949341afaf727b61b824
Someone <aayush.c2684@gmail.com>
2025-08-07 10:11:51 +0530

    Update initial positions of robot and glove to be at the origin for better alignment

--- eeg_robotic_arm_project/configs/main_config.py
+++ eeg_robotic_arm_project/configs/main_config.py
@@ -12,6 +12,6 @@ GLOVE_URDF_PATH = os.path.join(PROJECT_ROOT, "urdf", "glove.urdf")
 
 # Define the initial base position and orientation for both the robot and the glove.
 # For this visualization step, let's place them slightly apart on the Y-axis so we can see both clearly.
-ROBOT_START_POS = [0, -0.5, 0]
-GLOVE_START_POS = [0, 0.5, 0]
+ROBOT_START_POS = [0, 0, 0]
+GLOVE_START_POS = [0, 0, 0]
 START_ORIENTATION = [0, 0, 0, 1] # As a quaternion [x, y, z, w]
