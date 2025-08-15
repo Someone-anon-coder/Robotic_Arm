667059ac1e47b76c3bac9b2c81f385a3bb1a068e
Someone <aayush.c2684@gmail.com>
2025-08-15 16:13:36 +0530

    Completed Training and evaluation

--- eeg_rl_control/evaluate.py
+++ eeg_rl_control/evaluate.py
@@ -10,13 +10,19 @@ def main():
     model = SAC.load(ENV_CONFIG["model_save_path"])
 
     # Run evaluation loop
-    for episode in range(10):
+    for episode in range(5):
         obs, info = env.reset()
         terminated = False
         truncated = False
         while not terminated and not truncated:
             action, _states = model.predict(obs, deterministic=True)
             obs, reward, terminated, truncated, info = env.step(action)
+
+            # Get only first 5 and last 5 rewards in each episode
+            if env.step_counter < 2 or env.step_counter >= env.max_steps - 2:
+                print(f"Step: {env.step_counter}, Reward: {reward}")
+            
+        print(f"Episode {episode + 1} finished with reward: {reward}")
 
     # Close the environment
     env.close()
